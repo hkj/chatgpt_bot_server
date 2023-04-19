311999498fcad0bcd6df6fbff9f2b4dd17805138
hocnodon <315029+hkj@users.noreply.github.com>
2023-04-20 02:13:43 +0900

    Using Environment Variables and Exception Handling at server

--- server/main.py
+++ server/main.py
@@ -1,18 +1,22 @@
 import os
 import openai
 from fastapi import FastAPI
+import uvicorn
 
 app = FastAPI()
 
 @app.get("/q/{q_text}")
 async def chatgpt(q_text):
   openai.api_key = os.environ.get('OPENAI_API_KEY')
-  completion = openai.ChatCompletion.create(
-    model="gpt-3.5-turbo",
-    messages=[{"role": "user", "content": q_text}]
-  )
+  try:
+    completion = openai.ChatCompletion.create(
+      model="gpt-3.5-turbo",
+      messages=[{"role": "user", "content": q_text}]
+    )
+    return {"answer": completion.choices[0].message.content}
+  except Exception as e:
+    return {"answer": str(e)}
 
-  return {"answer": completion.choices[0].message.content}
-
-# if __name__ == '__main__':
-#   uvicorn.run("main:app", port=8000, reload=True)
+port = int(os.environ.get('SERVER_PORT'))
+if __name__ == '__main__':
+  uvicorn.run("main:app", port=port, reload=True, host="0.0.0.0")
