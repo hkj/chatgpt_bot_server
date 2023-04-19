fc3a41ed45933feacef3deb579ccaefdb91a1a29
hocnodon <315029+hkj@users.noreply.github.com>
2023-04-20 02:43:12 +0900

    Using Environment Variables and Exception Handling at bot

--- bot/app.py
+++ bot/app.py
@@ -9,13 +9,19 @@ app = App(token=os.environ.get("SLACK_BOT_TOKEN"))
 @app.event("app_mention")
 # mentionが来たら、ChatGPTサーバに送り、返答を貰う
 def message_mention(event, say):
-  url = 'http://localhost:8000/q/' + event["text"]
-  response = requests.get(url)
-  # 問題が無ければ、jsonを取得
-  if response.status_code == 200:
-    json = response.json()
+  try:
+    server_url = os.environ.get("SERVER_URL")
+    server_port = os.environ.get("SERVER_PORT")
+    url = f'{server_url}:{server_port}/q/' + event["text"]
+    # url = 'http://localhost:8000/q/' + event["text"]
+    response = requests.get(url)
+    # 問題が無ければ、jsonを取得
+    if response.status_code == 200:
+      json = response.json()
 
-  say(json['answer'])
+    say(json['answer'])
+  except Exception as e:
+    say("Some error occurred." + str(e))
 
 @app.event("message")
 def handle_message_events(body, logger):
