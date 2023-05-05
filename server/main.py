6dba6639efbfbc234c26621e671487c7615c28ec
hocnodon <315029+hkj@users.noreply.github.com>
2023-05-05 15:23:51 +0900

    changed model from gpt-3.5-turbo to gpt-4

--- server/main.py
+++ server/main.py
@@ -5,18 +5,21 @@ import uvicorn
 
 app = FastAPI()
 
+
 @app.get("/q/{q_text}")
 async def chatgpt(q_text):
-  openai.api_key = os.environ.get('OPENAI_API_KEY')
-  try:
-    completion = openai.ChatCompletion.create(
-      model="gpt-3.5-turbo",
-      messages=[{"role": "user", "content": q_text}]
-    )
-    return {"answer": completion.choices[0].message.content}
-  except Exception as e:
-    return {"answer": str(e)}
+    openai.api_key = os.environ.get("OPENAI_API_KEY")
+    try:
+        completion = openai.ChatCompletion.create(
+            #   model="gpt-3.5-turbo",
+            model="gpt-4",
+            messages=[{"role": "user", "content": q_text}],
+        )
+        return {"answer": completion.choices[0].message.content}
+    except Exception as e:
+        return {"answer": str(e)}
 
-port = int(os.environ.get('SERVER_PORT'))
-if __name__ == '__main__':
-  uvicorn.run("main:app", port=port, reload=True, host="0.0.0.0")
+
+port = int(os.environ.get("SERVER_PORT"))
+if __name__ == "__main__":
+    uvicorn.run("main:app", port=port, reload=True, host="0.0.0.0")
